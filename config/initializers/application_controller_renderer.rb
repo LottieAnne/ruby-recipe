3d37614f47dbcaef59b713a1bdb3c62e11066ead
cchaae <kkang_chung125@yahoo.co.jp>
2022-09-30 11:51:41 +0900

    feat: secret key追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd89acf186e2e9431a6161978eb4274db02c263f5588c54a6609c056261cba3070a0c34ac0aa50cf316bced182a2c6986f60b18e3e874d7dd2bb6e0f5aeeb5a48'
