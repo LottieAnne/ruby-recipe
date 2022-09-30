9916e21df2ff08e30e104fd45d9dd77265468846
cchaae <kkang_chung125@yahoo.co.jp>
2022-09-30 20:33:14 +0900

    feat: 削除のルーティング設定

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -28,6 +28,12 @@ class RecipesController < ApplicationController
     redirect_to recipe_path(@recipe)
   end
 
+  def destroy
+    recipe = Recipe.fing(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
   private
   def recipe_params
     params.require(:recipe).permit(:title, :body, :image)
