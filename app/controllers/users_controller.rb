ef323d988cd7dc8d1e39095aaae07fd6c4a1ce68
cchaae <kkang_chung125@yahoo.co.jp>
2022-10-03 11:26:11 +0900

    feat: 他人のユーザ編集アクセス制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,9 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to users_path, alert: "Unauthorized access"
+    end
   end
 
   def update
