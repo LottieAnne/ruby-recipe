9eb6efb7662b01912e9593a69aca030b6ffe9a02
cchaae <kkang_chung125@yahoo.co.jp>
2022-10-03 12:25:51 +0900

    fix: 軽微な修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to user_path(@user), notice: "Success!"
+      redirect_to user_path(@user), notice: "Update successfully!"
     else
       render :edit, status: :unprocessable_entity
     end
